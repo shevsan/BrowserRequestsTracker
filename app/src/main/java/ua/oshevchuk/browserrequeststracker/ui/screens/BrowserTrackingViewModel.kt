8f441068298d1e8d9674472f1f4d280d0725a2d2
oleksandrshevchuk <sasha41411@gmail.com>
2024-07-14 17:38:40 +0300

    Removed vm from service to avoid memory leaks, added 'start googling' button on empty screen

--- app/src/main/java/ua/oshevchuk/browserrequeststracker/ui/screens/BrowserTrackingViewModel.kt
+++ app/src/main/java/ua/oshevchuk/browserrequeststracker/ui/screens/BrowserTrackingViewModel.kt
@@ -1,6 +1,5 @@
 package ua.oshevchuk.browserrequeststracker.ui.screens
 
-import android.view.accessibility.AccessibilityNodeInfo
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,7 +9,6 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import ua.oshevchuk.browserrequeststracker.common.Response
 import ua.oshevchuk.browserrequeststracker.common.executeSafely
-import ua.oshevchuk.browserrequeststracker.domain.usecases.interfaces.AddRequestUseCase
 import ua.oshevchuk.browserrequeststracker.domain.usecases.interfaces.ClearAllUseCase
 import ua.oshevchuk.browserrequeststracker.domain.usecases.interfaces.DeleteRequestUseCase
 import ua.oshevchuk.browserrequeststracker.domain.usecases.interfaces.GetAllRequestsUseCase
@@ -19,7 +17,6 @@ import javax.inject.Inject
 
 @HiltViewModel
 class BrowserTrackingViewModel @Inject constructor(
-    private val addRequestUseCase: AddRequestUseCase,
     private val getAllRequestsUseCase: GetAllRequestsUseCase,
     private val deleteRequestUseCase: DeleteRequestUseCase,
     private val clearAllUseCase: ClearAllUseCase
@@ -91,37 +88,6 @@ class BrowserTrackingViewModel @Inject constructor(
         }
     }
 
-
-    fun extractUrl(nodeInfo: AccessibilityNodeInfo?): String? {
-        if (nodeInfo == null) return null
-
-        var currentNode = nodeInfo
-        while (currentNode != null) {
-            if (currentNode.className == "android.webkit.WebView" || currentNode.className == "android.widget.EditText") {
-                val url = currentNode.text?.toString()
-                if (url != null) {
-                    return url
-                }
-            }
-
-            currentNode = currentNode.parent
-        }
-
-        return null
-    }
-
-    fun saveUrlToDatabase(url: String, timestamp: Long) {
-        viewModelScope.launch {
-            executeSafely {
-                addRequestUseCase(
-                    BrowserRequestEntity(
-                        url = url,
-                        timestamp = timestamp
-                    )
-                )
-            }
-        }
-    }
 
 
 }
--- app/src/main/java/ua/oshevchuk/browserrequeststracker/ui/service/BrowserTrackingService.kt
+++ app/src/main/java/ua/oshevchuk/browserrequeststracker/ui/service/BrowserTrackingService.kt
@@ -3,36 +3,47 @@ package ua.oshevchuk.browserrequeststracker.ui.service
 import android.accessibilityservice.AccessibilityService
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelStore
 import dagger.hilt.android.AndroidEntryPoint
-import ua.oshevchuk.browserrequeststracker.ui.screens.BrowserTrackingViewModel
-import ua.oshevchuk.browserrequeststracker.ui.factories.BrowserTrackingViewModelFactory
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import ua.oshevchuk.browserrequeststracker.common.executeSafely
+import ua.oshevchuk.browserrequeststracker.common.extractUrl
+import ua.oshevchuk.browserrequeststracker.di.annotations.IoDispatcher
+import ua.oshevchuk.browserrequeststracker.domain.usecases.interfaces.AddRequestUseCase
+import ua.oshevchuk.browserrequeststracker.ui.enitites.BrowserRequestEntity
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class BrowserTrackingService : AccessibilityService() {
 
     @Inject
-    lateinit var viewModelFactory: BrowserTrackingViewModelFactory
-    private lateinit var viewModel: BrowserTrackingViewModel
+    @IoDispatcher
+    lateinit var ioDispatcher: CoroutineDispatcher
+    private lateinit var scope: CoroutineScope
 
-    override fun onCreate() {
-        super.onCreate()
-        viewModel = ViewModelProvider(store = ViewModelStore(), factory = viewModelFactory)[BrowserTrackingViewModel::class.java]
-    }
+    @Inject
+    lateinit var addRequestUseCase: AddRequestUseCase
+
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        scope = CoroutineScope(ioDispatcher + SupervisorJob())
+
         event?.let {
             if (event.eventType == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED ||
                 event.eventType == AccessibilityEvent.TYPE_VIEW_SCROLLED
             ) {
                 val nodeInfo = event.source ?: return
-                val url = viewModel.extractUrl(nodeInfo)
+                val url = extractUrl(nodeInfo)
 
                 url?.let {
                     if (it.contains("google.com")) {
-                        viewModel.saveUrlToDatabase(it, System.currentTimeMillis())
+                        scope.launch {
+                            executeSafely {
+                                addRequestUseCase(BrowserRequestEntity(url = it, timestamp = System.currentTimeMillis()))
+                            }
+                        }
                     }
                 }
             }
